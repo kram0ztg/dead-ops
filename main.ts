77daa90a61bd5740a430f7516d62b2f963322447
kram0ztg <156235879+kram0ztg@users.noreply.github.com>
2024-01-23 12:03:56 -0600

    Restore '1/23/2024, 11:56:48 AM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ function Player2Score (P2Score: any[]) {
 function Player1Score (P1Score: any[]) {
 	
 }
+let list: number[] = []
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -42,15 +43,5 @@ let mySprite2 = sprites.create(img`
     `, SpriteKind.Player)
 info.player1.setScore(0)
 info.player2.setScore(0)
-// Score Changes
-forever(function () {
-    let list: number[] = []
-    // Will call score when P1 hit target
-    if (true) {
-        Player1Score(list)
-    }
-    // Will call score when P1 hit target
-    if (true) {
-        Player2Score(list)
-    }
-})
+Player1Score(list)
+Player2Score(list)
